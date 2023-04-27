34d26a19acb4755fb56a316ab846f3e6eacc5d6a
Coriana Williams <corianacory@gmail.com>
2023-04-26 21:02:26 -0400

    fixed deleted and added put

--- seed.js
+++ seed.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const mongoose = require('mongoose');
-
 require('dotenv').config();
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URL);
 
 const Books = require('./models/books.js');
--- server.js
+++ server.js
@@ -93,6 +93,28 @@ async function deleteBooks (request, response, next) {
   }
   }
 
+  //TODO: ENDPOINT TO UPDATE BOOKS LAB 13
+
+  app.put('/booksID/:booksID', updateBooks);
+
+  async function updateBooks(request, response, next) {
+    try {
+// TODO: GRAB THE ID FROM THE REQUEST.PARAMS & DATA FROM REQUEST.BODY LAB 13
+let id = request.params.booksID;
+let booksData = request.body;
+
+//TODO: USE MODEL METHOD OF FINDBYIDANDUPDATE AND PASS TO ID, DATA, OPTIONS OBJECT LAB 13 
+//NEED 3 ARGUEMENT (ID, DATA, OPTIONS OBJECTS -> {new: true, overwrite: true})
+let updatedBooks = await Books.findByIdAndUpdate(id, booksData, {new: true, overwrite: true});
+
+//TODO: SEND ON RESPONCE, THE UDATED BOOKS LAB 13
+  response.status(200).send(updatedBooks);
+
+    } catch (error) {
+      next(error)
+    }
+  }
+
 app.get('*', (request, response) => {
   response.status(404).send('Not available');
 });
